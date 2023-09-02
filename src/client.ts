c349631b12fa55be46e451cc500f2665ee9d0edf
konrad <king.koxy@gmail.com>
2023-09-02 23:52:23 +0200

    fix

--- src/client.ts
+++ src/client.ts
@@ -1,27 +1,8 @@
-import { HoudiniClient, type RequestHandlerArgs } from '$houdini';
-
-const headers: Record<string, string> = {
-	'Content-Type': 'application/json',
-	Authorization: `Bearer ${import.meta.env.CONTENTFUL_ACCESS_TOKEN}`
-};
-
-async function fetchQuery({ fetch, text = '', variables = {}, metadata }: RequestHandlerArgs) {
-	const url =
-		'https://graphql.contentful.com/content/v1/spaces/' + import.meta.env.CONTENTFUL_SPACE_ID;
-	const result = await fetch(url, {
-		method: 'POST',
-		headers: headers,
-		body: JSON.stringify({
-			query: text,
-			variables
-		})
-	});
-	return await result.json();
-}
+import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
 	url: 'https://graphql.contentful.com/content/v1/spaces/' + import.meta.env.CONTENTFUL_SPACE_ID,
-	fetchParams({}) {
+	fetchParams() {
 		return {
 			headers: {
 				'Content-Type': 'application/json',
