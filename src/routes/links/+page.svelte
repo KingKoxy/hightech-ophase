f7db8565a3b0d5a73b3be54e9dc50fc5cb34fe34
Konrad <king.koxy@gmail.com>
2024-10-03 18:32:34 +0200

    updated link page

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -7,28 +7,70 @@
 	<h2 class="text-xl font-semibold mb-2">In der O-Phase:</h2>
 	<ul class="ml-4">
 		<li>
-			Die offizielle <a href="https://o-phase.com/de/">Website</a> der Fachschaft Mathe/Info zur
-			O-Phase
+			Die offizielle <a href="https://o-phase.com/de/">Website</a> der Fachschaft Mathe/Info zur O-Phase
 		</li>
 		<li>
-			<a href="https://www.asta-kit.de/de/notunterkuenfte">AStA-Notunterkünfte</a> - Unterkünfte im
-			HaDiKo (Oststadt) für alle, die während der O-Phase noch keinen Schlafplatz in KA haben
+			<a href="https://web1.karlsruhe.de/service/Buergerdienste/leistung.php?id=6021867"
+				>Wohnsitz anmelden</a
+			> - Wenn du nach Karlsruhe gezogen bist, musst du dich innerhalb von 14 Tagen beim Einwohnermeldeamt
+			anmelden
+		</li>
+		<li>
+			<a href="https://www.bafoeg-digital.de/ams/BAFOEG">Online BAföG-Antrag</a> - Hier kannst du deinen
+			BAföG-Antrag online stellen. Mach das am besten so schnell wie möglich, da die Bearbeitung einige
+			Zeit dauern kann
+		</li>
+		<li>
+			<a href="https://www.asta-kit.de/de/notunterkuenfte">AStA-Notunterkünfte</a> - Unterkünfte im HaDiKo
+			(Oststadt) für alle, die während der O-Phase noch keinen Schlafplatz in KA haben
 		</li>
 	</ul>
 </section>
 <section class="mb-5">
-	<h2 class="text-xl font-semibold mb-2">Im Studium:</h2>
+	<h2 class="text-xl font-semibold mb-2">Für das Studium:</h2>
 	<ul class="ml-4">
-<li>
-			<a href="https://bubatzkarte.de/#14/49.0154/8.4151">Bubatzkarte</a> - Karte mit Zonen in denen Cannabis konsumiert werden darf
+		<li>
+			<a
+				href="https://ilias.studium.kit.edu/ilias.php?baseClass=ilrepositorygui&reloadpublic=1&cmd=frameset&ref_id=1"
+				>Ilias</a
+			> - Hier werden Vorlesungsunterlagen zu den meisten Fächern hochgeladen
 		</li>
 		<li>
 			<a href="https://campus.studium.kit.edu/">Campus Management Portal</a> - Bescheinigungen wie das
 			KVV Ticket ausdrucken, Prüfungsanmeldung, etc.
 		</li>
 		<li>
+			<a
+				href="https://campus.studium.kit.edu/events/catalog.php#!campus/all/fields.asp?group=Vorlesungsverzeichnis"
+				>Vorlesungsverzeichnis</a
+			> - Hier findet ihr alle Lehrveranstaltungen
+		</li>
+		<li>
 			<a href="https://www.fsmi.uni-karlsruhe.de/odie/web/">Odie</a> - Hier findest du eine große Sammlung
-			an Altklausuren
+			an Altklausuren und Gedächtnisprotokollen
+		</li>
+		<li>
+			<a href="https://discord.gg/D3RCCMj">Discordserver <i>Mathe/Info KIT</i></a> - Discordserver der
+			Fachschaft. Hier kannst du Fragen stellen und dich mit anderen Studierenden austauschen. Außerdem
+			findet man hier auch oft Zusammenfassungen und Altklausuren
+		</li>
+		<li>
+			<a href="https://discord.gg/sGwwnXAv">Discordserver <i> KIT Studierende</i></a> - Ein Discordserver
+			für alle Studierenden am KIT
+		</li>
+		<li>
+			<a href="https://codetester.ialistannen.de/">Simple Code Tester</a> - Eine inoffizielle Seite von
+			KIT-Studierenden, die ihr verwenden könnt, um eure Abgabe für die Programmieren-Übungsblätter zu
+			testen
+		</li>
+		<li>
+			<a href="https://www.asta-kit.de/de/angebote/druckerei">AStA-Druckerei</a> - Hier könnt ihr alles
+			mögliche sehr günstig drucken lassen. Egal, ob Abschlussarbeiten, Poster, Visitenkarten oder sogar
+			Textilien
+		</li>
+		<li>
+			<a href="https://www.bibliothek.kit.edu/">KIT-Bibliothek</a> - Hier kann man viele Bücher kostenlos
+			herunterladen
 		</li>
 		<li>
 			<a href="https://www.kit.edu/campusplan/">Karte</a> für den Campus mit Gebäudenummern (als
@@ -38,44 +80,8 @@
 			>)
 		</li>
 		<li>
-			<a
-				href="https://campus.studium.kit.edu/events/catalog.php#!campus/all/fields.asp?group=Vorlesungsverzeichnis"
-				>Vorlesungsverzeichnis</a
-			> - Hier findet ihr alle Lehrveranstaltungen
-		</li>
-		<li>
-			<a href="https://opencast.informatik.kit.edu/engage/ui/index.html">Opencast</a> - Hier wird ein
-			Teil der Vorlesungen für Informatiker hochgeladen
-		</li>
-		<li>
-			<a
-				href="https://ilias.studium.kit.edu/ilias.php?baseClass=ilrepositorygui&reloadpublic=1&cmd=frameset&ref_id=1"
-				>Ilias</a
-			> - Hier werden Vorlesungsunterlagen zu den meisten Fächern hochgeladen
-		</li>
-		<li>
-			<a href="https://www.sle.kit.edu/imstudium/termine-fristen.php">Termine und Fristen</a> - Übersicht
-			der Dauer von Vorlesungszeit und Prüfungszeitraum
-		</li>
-		<li>
-			<a href="https://www.bibliothek.kit.edu/">KIT-Bibliothek</a> - Hier kann man viele Bücher kostenlos
-			herunterladen
-		</li>
-		<li>
-			<a href="https://www.reddit.com/r/KaIT/">KIT Subreddit</a> - Memes
-		</li>
-		<li>
-			<a href="https://discord.gg/D3RCCMj">Discord Mathe/Info KIT</a> - Discord Server der Fachschaft
-		</li>
-		<li>
-			<a
-				href="https://docs.google.com/spreadsheets/d/1cl-YATVcFwmUglBBJFDRW7CtSt_1gNaWOFztWMAak24/edit#gid=0"
-				>Spreadsheet</a
-			> mit PWs zu Vorlesungen
-		</li>
-		<li>
-			<a href="https://www.fsmi.uni-karlsruhe.de/">Fachschaftsseite</a> - Hier lassen sich u.a. Klausuren
-			ausdrucken
+			<a href="https://www.fsmi.uni-karlsruhe.de/">Fachschaftsseite</a> - Die Website der Fachschaft
+			Mathematik/Informatik
 		</li>
 		<li>
 			<a href="https://www.fsmi.uni-karlsruhe.de/Links.html"
@@ -84,43 +90,63 @@
 		</li>
 	</ul>
 </section>
+
 <section class="mb-5">
-	<h2 class="text-xl font-semibold mb-2">Facebookgruppen für Informatik und Mathematik:</h2>
-	Hier wird oft etwas zu kommenden Klausuren gefragt. Es empfiehlt sich einen FB-Account anzulegen, falls
-	man keinen hat.
-	<ul class="ml-4 mt-2">
+	<h2 class="text-xl font-semibold mb-2">Für das Studierendenleben:</h2>
+	<ul class="ml-4">
 		<li>
-			<a href="https://www.facebook.com/groups/477441895751127">KIT Informatik Studierende</a>
+			<a href="https://www.sw-ka.de/de/"> Studierendenwerk Karlsruhe </a> - Zuständig unter anderem für
+			die Mensen, Wohnheime und BAföG. Bietet außerdem Beratung an und organisiert eine Jobbörse für
+			Studierende
 		</li>
 		<li>
-			<a href="https://www.facebook.com/groups/mathe.info.kit">Mathe-Informatik Gruppe</a>
+			<a href="https://www.asta-kit.de/de/content/allgemeiner-studierendenausschuss-asta">AStA</a> -
+			Der allgemeine Studierendenausschuss des KIT. Sie vertreten die Studierenden gegenüber der Hochschulleitung
+			und sind eine Anlaufstelle für alle studentischen Belange und Probleme
+		</li>
+		<li>
+			<a href="https://www.akk.org/">AKK</a> - Der Arbeitskreis Kultur und Kommunikation. Sie organisieren
+			unter anderem das campuseigene Kultur-Café, veranstalten regelmäßig Events und bieten Tanzkurse
+			und Workshops an
+		</li>
+		<li>
+			<a href="https://www.asta-kit.de/de/angebote/fahrzeuge">AStA-Fahrzeugvermietung</a> - Die AStA
+			vermietet kostengünstig Transporter an Studierende. Das ist zum Beispiel für Umzüge sehr nützlich
+		</li>
+		<li>
+			<a href="https://www.reddit.com/r/KaIT/">KIT Subreddit</a> - Memes über das Studium am KIT
+		</li>
+		<li>
+			<a href="https://bubatzkarte.de/#14/49.0154/8.4151">Bubatzkarte</a> - Karte mit Zonen in denen
+			Cannabis konsumiert werden darf
 		</li>
 	</ul>
 </section>
 <section class="mb-5">
-	<h2 class="text-xl font-semibold mb-2">Lernen:</h2>
+	<h2 class="text-xl font-semibold mb-2">Naturfreunde am KIT:</h2>
+	Die Naturfreunde sind eine Hochschulgruppe, in der sich viele der HiGHtech-Tutoren engagieren.
+	Sie organisieren z.B. regelmäßig Wanderungen, Müllsammelaktionen und Raves in und um Karlsruhe.
 	<ul class="ml-4 mt-2">
 		<li>
-			<a href="https://www.youtube.com/c/3blue1brown">3Blue1Brown</a>
-			- Guter YT-Kanal für Intuition bei
-			<a href="https://www.youtube.com/watch?v=kjBOesZCoqc&list=PL0-GT3co4r2y2YErbmuJw2L5tW4Ew2O5B"
-				>LA</a
-			>
-			und
-			<a href="https://www.youtube.com/watch?v=WUvTyaaNkzM&list=PL0-GT3co4r2wlh6UHTUeQsrf3mlS2lk6x"
-				>HM</a
-			>
+			<a href="https://www.instagram.com/naturfreunde.kit">Instagram</a>
 		</li>
 		<li>
-			<a href="https://www.youtube.com/c/MathePeter/">MathePeter</a> - Gut erklärte Videos für Rechenverfahren
+			<a href="https://www.youtube.com/@NaturfreundeamKIT/videos">YouTube</a>
+		</li>
+		<li>
+			<a href="https://facebook.com/Naturfreundeamkit/">Facebook</a>
 		</li>
 	</ul>
 </section>
+
 <section class="mb-5">
 	<h2 class="text-xl font-semibold mb-2">Unsere DJs:</h2>
 	<ul class="ml-4 mt-2">
 		<li>
 			<a href="https://soundcloud.com/luis-reissenweber">Luis Reissenweber</a>
+		</li>
+		<li>
+			<a href="https://soundcloud.com/dj_julicorn">Julicorn</a>
 		</li>
 		<li>
 			<a href="https://soundcloud.com/ilya_filippov">Ilya Filippov</a>
@@ -132,16 +158,20 @@
 			<a href="https://soundcloud.com/juliuspfister"> Julius Pfister </a>
 		</li>
 		<li>
+			<a href="https://soundcloud.com/mete_mete"> METE</a>
+		</li>
+		<li>
+			<a href="https://soundcloud.com/itsrerz"> rerz </a>
+		</li>
+		<li>
 			<a href="https://soundcloud.com/user-601516904"> Kobaltikum </a>
 		</li>
 		<li>
 			<a href="https://soundcloud.com/electra-twins"> Electra Twins </a>
 		</li>
-		<li>
-			<a href="https://soundcloud.com/itsrerz"> rerz </a>
-		</li>
 	</ul>
 </section>
+
 <!--<section>
 	<h2 class="text-xl font-semibold mb-2">Weitere Links:</h2>
 	<ul class="ml-4 mt-2">
